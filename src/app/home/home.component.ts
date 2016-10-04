c1f8d803a7183029f299537e55c2fd02b25cd291
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-10-05 00:53:10 +0200

    stuff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import {Component} from '@angular/core';
 import {AppState} from '../app.service';
+import * as io from 'socket.io-client';
 
 import {Title} from './services/title';
 import {XLarge} from './directives/x-large';
 
 import {Accordion} from '../shared/components/accordion/accordion.component';
 
-import {AccordionGroup} from
+import {
+  AccordionGroup
+} from
   '../shared/components/accordion/accordion-group.component';
 
 // Import NgFor directive
@@ -24,21 +27,23 @@ import {NgFor} from '@angular/common';
   // We need to tell Angular's compiler which directives are in our template.
   // Doing so will allow Angular to attach our behavior to an element
   directives: [
-      XLarge,
-      Accordion,
-      AccordionGroup,
-      NgFor
+    XLarge,
+    Accordion,
+    AccordionGroup,
+    NgFor
   ],
   // We need to tell Angular's compiler which custom pipes are in our template.
-  pipes: [ ],
+  pipes: [],
   // Our list of styles in our component. We may add more to compose many styles together
-  styles: [ require('./home.css') ],
+  styles: [require('./home.css')],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   template: require('./home.html')
 })
 export class Home {
+
   // Set our default values
-  localState = { value: '' };
+  localState = {value: ''};
+  socket:any = null;
 
   // TypeScript public modifiers
   constructor(public appState: AppState, public title: Title) {
@@ -48,22 +53,23 @@ export class Home {
   isOpen: boolean = false;
 
   groups: Array<any> = [
-      {
-          heading: 'Dynamic 1',
-          content: 'I am dynamic!'
-      },
-      {
-          heading: 'Dynamic 2',
-          content: 'Dynamic as well'
-      }
+    {
+      heading: 'Dynamic 1',
+      content: 'I am dynamic!'
+    },
+    {
+      heading: 'Dynamic 2',
+      content: 'Dynamic as well'
+    }
   ];
 
   removeDynamic() {
-      this.groups.pop();
+    this.groups.pop();
   }
 
   ngOnInit() {
     console.log('hello `Home` component');
+    this.socket = io('http://localhost:1337');
     // this.title.getData().subscribe(data => this.data = data);
   }
 
@@ -72,4 +78,7 @@ export class Home {
     this.appState.set('value', value);
     this.localState.value = '';
   }
+
+
+
 }
